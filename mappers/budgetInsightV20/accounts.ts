49461be15ac8dfa090688c81e735331057774449
ccoeurderoy <charles.coeurderoy@algoan.com>
2022-04-22 16:51:32 +0200

    feat: create a converter from Algoan to BI format

---
+++ mappers/budgetInsightV20/accounts.ts
@@ -0,0 +1,72 @@
+/**
+ * Map function to convert an Algoan Account to BI format
+ */
+export const algoanAccountsToBIAccounts = (): any => {
+  return (account: any) => ({
+    transactions: account.transactions.map(algoanTransactions()),
+    balance: account.balance,
+    last_update: account.balanceDate,
+    currency: {
+      id: 'EUR'
+    },
+    type: getBIType(account.type),
+    usage: getBIUsage(account.usage),
+    id: Math.floor(Math.random() * 1000),
+  });
+}
+
+/**
+ * Map function to convert an Algoan Account to BI format
+ */
+export const algoanTransactions = (): any => {
+  return (transaction: any) => {
+    return {
+      date: transaction.dates.debitedAt,
+      rdate: transaction.dates.debitedAt,
+      currency: transaction.currency,
+      value: transaction.amount,
+      original_wording: transaction.description,
+      id: Math.floor(Math.random() * 1000),
+    }
+  }
+}
+
+/**
+ * Get Budget Insight account type
+ * @param accountType Algoan type
+ */
+function getBIType(accountType: string) {
+  switch(accountType) {
+    case 'CHECKING':
+      return 'checking';
+
+    case 'SAVINGS':
+      return 'savings'
+
+    case 'LOAN':
+      return 'loan';
+
+    case 'CREDIT_CARD':
+      return 'card'
+
+    default:
+      return 'unknown';
+  }
+}
+
+/**
+ * Get Budget Insight account usage
+ * @param usage Algoan usage 
+ */
+function getBIUsage(usage: string) {
+  switch(usage) {
+    case 'PERSONAL':
+      return 'PRIV';
+
+    case 'PROFESSIONAL':
+      return 'ORGA';
+
+    default:
+      return 'ASSO';
+  }
+}
---
+++ scripts/converter.ts
@@ -0,0 +1,65 @@
+import * as path from 'path';
+
+import { readJSONFiles, writeJSONFiles } from '../lib/utils';
+import { algoanAccountsToBIAccounts } from '../mappers/budgetInsightV20/accounts';
+import { FileEntity } from '../types';
+
+const formats: string[] = ['BUDGET_INSIGHT_V2_0'];
+
+/**
+ * Convert an Algoan data sample to another format
+ * @param format supported format
+ * @param data data to convert
+ */
+function convertAlgoanToAnotherFormat(format: string, data: any) {
+  const convertedSample: any = {
+    format,
+  };
+
+  switch (format) {
+    case 'BUDGET_INSIGHT_V2_0':
+      convertedSample.connections = [];
+      convertedSample.connections.push({
+        accounts: data.accounts.map(algoanAccountsToBIAccounts()),
+      });
+      break;
+
+    default:
+      throw new Error(`${format} is an unknown format`);
+  }
+
+  return convertedSample;
+}
+
+(async () => {
+  /**
+   * Load JSON files
+   */
+  const sampleDirectoryPath: string = path.join(__dirname, '..', 'samples/fr/');
+  const fileEntities: FileEntity[] = readJSONFiles(sampleDirectoryPath);
+  const convertedFiles: FileEntity[] = [];
+
+  for (const format of formats) {
+    const dirPath: string = path.join(__dirname, '..', `${format.toLocaleLowerCase()}/fr`);
+    /**
+     * For each file, apply the proper translation depending on the format
+     */
+    fileEntities.forEach((filePath: FileEntity) => {
+      const convertedSample = convertAlgoanToAnotherFormat(format, filePath.sample);
+
+      convertedFiles.push({
+        filename: filePath.filename,
+        sample: convertedSample,
+      });
+    });
+
+    /**
+     * Save files
+     */
+    writeJSONFiles(convertedFiles, dirPath);
+  }
+})().catch((err: Error) => {
+  console.error(err);
+
+  process.exit(1);
+});
